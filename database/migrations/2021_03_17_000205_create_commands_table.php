f845b8baf711453f1404c12c51a235208319f9e8
Pericles Adjovi <periclesadjovi@outlook.fr>
2021-03-17 01:27:45 +0100

    first commit

---
+++ database/migrations/2021_03_17_000205_create_commands_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommandsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('commands', function (Blueprint $table) {
+            $table->bigIncrements('id')
+            $table->bigIncrements('user_id');
+            $table->bigIncrements('car_id');
+            $table->datetime('dateL');
+            $table->datetime('dateR');
+            $table->varchar('prixTTC');
+            $table->notes('text');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('commands');
+    }
+}
