1cb914d65f78ff62ca80dac760de47b2b606c506
Pericles Adjovi <periclesadjovi@outlook.fr>
2021-03-17 11:33:14 +0100

    working on layouts

--- resources/views/cars/index.blade.php
+++ resources/views/cars/index.blade.php
@@ -1,10 +1,9 @@
 @extends('layouts.master')
 
-
 @section('content')
 
 <div class="row my-4">
-    <div class="col-md-4">
+    <div class="col-md-3">
         <div class="card bg-light border border-primary">
             <h3 class="card-header">
                 Recherche
@@ -15,14 +14,59 @@
 <label for="search">Recherche</label>
 <input type="text" name="search" id="" class="form-control" placeholder="Recherche..."/>
 
-                            </div>
+             </div>
 
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary">Submit</button>
-                              </div>
+                 <div class="form-group">
+                     <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
                 </form>
                 </div>
         </div>
     </div>
+
+<div class="col-md-6">
+
+<div class="border border-primary">
+
+        <h3 class="card-header">Toutes les voitures</h3>
+        <div class="card-body">
+           @foreach($cars as $car)
+
+           <div class="media">
+            <div class="media-left">
+                <img src="{{$car->image}}"    alt="" class="img-fluid rounded-circle">
+            </div>
+
+
+            <div class="media-body">
+                <h3 class="text-info">
+                    <a href="" class="btn btn-link">
+                        {{$car->marque}}
+
+                    </a>
+                </h3>
+                
+                 <p class="d-flex flex-row justify-content-between align-items-center">
+                    <span class="badge badge-danger">
+                        {{$car->type}}
+                    </span>
+                    <span class="badge badge-primary">
+                        {{$car->prixJ}}
+                    </span>
+                </p>
+            </div>
+            </div>
+
+
+           @endforeach
+
+        </div>
+
+</div>
+
+
+
+</div>
+
 </div>
 @endsection
