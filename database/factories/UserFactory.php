604de4756ace13428418a603939ebb0b1bb650e0
Pericles Adjovi <periclesadjovi@outlook.fr>
2021-03-17 04:36:50 +0100

    ...

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -25,6 +25,9 @@ class UserFactory extends Factory
         return [
             'name' => $this->faker->name,
             'email' => $this->faker->unique()->safeEmail,
+            'tel' => $this->faker->phoneNumber,
+            'ville' => $this->faker->city,
+            'notes' => $this->faker->sentences(rand(1, 3)),
             'email_verified_at' => now(),
             'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
             'remember_token' => Str::random(10),
