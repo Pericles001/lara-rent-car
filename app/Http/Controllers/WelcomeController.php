92411be74e0c79801e3fc4eeebf3b9ff8db4b9ed
pericles001 <periclesadjovi@outlook.fr>
2022-02-03 12:56:29 +0100

    Adding new package to notify users

---
+++ app/Http/Controllers/WelcomeController.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Http\Controllers\Auth;
+
+use Spatie\WelcomeNotification\WelcomeController as BaseWelcomeController;
+
+class MyWelcomeController extends BaseWelcomeController
+{
+}
--- app/Models/User.php
+++ app/Models/User.php
@@ -6,7 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-
+use \Spatie\WelcomeNotification\ReceivesWelcomeNotification;
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
